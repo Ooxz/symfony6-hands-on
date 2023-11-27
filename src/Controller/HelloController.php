a53be57fa0f73aedef1bb7e9bc4e6d72afe67c95
Adrien Brune <adrienbrune.pro@gmail.com>
2023-11-27 17:35:13 +0100

    implement twig templates

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,35 +2,36 @@
 
 namespace App\Controller;
 
-
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class HelloController 
+class HelloController extends AbstractController
 {
-  // An array of strings that can be printed to the user
-  private array $messages = [ 'Hello', 'Hi', 'Bye' ];
+  private array $messages = [
+    "Hello", "Hi", "Bye!"
+  ];
 
-  // This line defines the route for the index() method.
-  // When the server gets a request at the root of the website ("/"), it uses this method.
-  #[Route('/', name: 'app_index')]  
-  public function index(): Response
+  #[Route('/{limit?3}', name: 'app_index')]
+  public function index($limit): Response
   {
-    // This method creates a new HTTP response that contains all the messages, 
-    // concatenated together with commas in between, and returns it.
-    return new Response(implode(',', $this->messages));    
-  } 
+    $limit = (int)$limit;  // this will convert the limit to integer
+    return $this->render(
+      'hello/index.html.twig',
+      [
+        'message' => implode(',', array_slice($this->messages, 0, $limit))
+      ]
+    );
+  }
 
-  // This line defines the route for the showOne() method.
-  // When the server gets a request for "/messages/{id}" (where {id} can be any string), 
-  // it uses this method.
-  #[Route('/messages/{id}', name: 'app_show_one')]
-  public function showOne($id): Response
+  #[Route('/messages/{id<\d+>}', name: 'app_show_one')]
+  public function showOne(int $id): Response
   {
-    // This method creates a new HTTP response that contains the message at the 
-    // index specified by {id}, then returns the HTTP response.
-    // If the {id} given in the URL does not correspond with an index in $messages, it will throw an error.
-    return new Response($this->messages[$id]);
+    return $this->render(
+      'hello/show_one.html.twig',
+      [
+        'message' => $this->messages[$id]
+      ]
+    );
   }
 }
-
